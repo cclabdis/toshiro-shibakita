ae8b3c545d2d98b5ee4260787d88bc5e824390e1
cclabdis <cclabdis@gmail.com>
2023-03-03 03:47:19 -0300

    finalização do desafio

--- banco.sql
+++ banco.sql
@@ -1,8 +1,37 @@
-CREATE TABLE dados (
-    AlunoID int,
-    Nome varchar(50),
-    Sobrenome varchar(50),
-    Endereco varchar(150),
-    Cidade varchar(50),
-    Host varchar(50)
+-- Tabela de lojas
+CREATE TABLE lojas (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(50) NOT NULL
+);
+
+-- Inserindo 10 lojas aleatórias
+INSERT INTO lojas (nome) VALUES
+('Loja A'),
+('Loja B'),
+('Loja C'),
+('Loja D'),
+('Loja E'),
+('Loja F'),
+('Loja G'),
+('Loja H'),
+('Loja I'),
+('Loja J');
+
+-- Tabela de clientes
+CREATE TABLE clientes (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(50) NOT NULL,
+  sobrenome VARCHAR(50) NOT NULL,
+  idade INT NOT NULL,
+  telefone VARCHAR(20) NOT NULL
+);
+
+-- Tabela de preferências dos clientes
+CREATE TABLE preferencias (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  cliente_id INT NOT NULL,
+  loja_id INT NOT NULL,
+  servico VARCHAR(50) NOT NULL,
+  FOREIGN KEY (cliente_id) REFERENCES clientes(id),
+  FOREIGN KEY (loja_id) REFERENCES lojas(id)
 );
--- index.php
+++ index.php
@@ -34,7 +34,13 @@ $valor_rand2 = strtoupper(substr(bin2hex(random_bytes(4)), 1));
 $host_name = gethostname();
 
 
-$query = "INSERT INTO dados (AlunoID, Nome, Sobrenome, Endereco, Cidade, Host) VALUES ('$valor_rand1' , '$valor_rand2', '$valor_rand2', '$valor_rand2', '$valor_rand2','$host_name')";
+$query = "INSERT INTO clientes 
+  ( id,
+  nome,
+  sobrenome,
+  idade,
+  telefone) 
+VALUES ('$valor_rand1' , '$valor_rand2', '$valor_rand2', '$valor_rand2', '$valor_rand2','$host_name')";
 
 
 if ($link->query($query) === TRUE) {
